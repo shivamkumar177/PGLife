b1af88564692d715ff4435a98c32cbc158a4b08c
shivam <shivam.k@crediwatch.com>
2022-05-03 15:34:34 +0530

    Added inital build

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi import APIRouter
+ 
+app = FastAPI()
+
+api_router = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/templates", StaticFiles(directory="templates"), name="templates")
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("dashboard.html", {"request":request})
